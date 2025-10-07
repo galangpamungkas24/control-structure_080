739f4e6fdd5ae923571702c4f16713767252f759
galang <ahmad.galang.pamungkas24@gmail.com>
2025-10-07 15:23:21 +0700

    tiga

---
+++ prak3.py
@@ -0,0 +1,8 @@
+ n = int(input("masukkan nilai n untuk batas deret fibonanci: "))
+
+a, b = 0, 1
+
+print("deret fibonanci hingga", n, ":")
+while a <= n:
+    print(a, end=" ")
+    a, b = b, a + b
