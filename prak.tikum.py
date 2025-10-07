199da06a5723783dbedd8641826d756f83b8a674
galang <ahmad.galang.pamungkas24@gmail.com>
2025-10-07 15:06:35 +0700

    first

---
+++ prak.tikum.py
@@ -0,0 +1,8 @@
+ percentage = float (input("masukan persentase nilai siswa"))
+
+ if percentage >= 90:
+    print ("excelent perfomance")
+ elif percentage >= 80:
+    print ("very good perfomance")
+ elif percentage >= 70:
+    print()      
